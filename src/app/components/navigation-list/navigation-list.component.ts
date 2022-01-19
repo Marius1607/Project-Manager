73b077ee5628b6c87f341660a93a37bf36caf3c3
Marius Bijan <marius.bijan33@gmail.com>
2022-01-19 12:54:17 +0200

    switch added

--- src/app/components/navigation-list/navigation-list.component.ts
+++ src/app/components/navigation-list/navigation-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArrayUtilitiesService } from 'src/app/services/array-utilities.service';
 
 @Component({
   selector: 'app-navigation-list',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationListComponent implements OnInit {
 
-  constructor() { }
+  switch = ["switch"];
+  switch_frame = ["switch-frame"];
 
+  constructor(private array_utilities: ArrayUtilitiesService) { }
+  
   ngOnInit(): void {
+  }
+
+  switchOn(): void {
+    if (!this.array_utilities.element_exists(this.switch, "flip-the-switch")) {
+      this.array_utilities.remove_element(this.switch, 'unflip-the-switch');
+      this.switch.push("flip-the-switch");
+    }
+    else {
+      this.array_utilities.remove_element(this.switch, 'flip-the-switch');
+      this.switch.push('unflip-the-switch');
+    }
   }
 
 }
--- src/app/services/array-utilities.service.ts
+++ src/app/services/array-utilities.service.ts
@@ -8,11 +8,16 @@ export class ArrayUtilitiesService {
   constructor() { }
 
   public remove_element(elements: Array<string>, element: string) {
-
     var index = elements.indexOf(element);
 
     if (index > -1) {
        elements.splice(index, 1);
     }
+  }
+
+  public element_exists(elements: Array<string>, element: string) {
+    var index = elements.indexOf(element);
+    
+    return index > -1;
   }
 }
