9f96c99559463feefb933c69677239b4797a5400
Marius Bijan <marius.bijan33@gmail.com>
2022-01-19 10:54:50 +0200

    branch syncronized to master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VerticalListComponent } from './components/vertical-list/vertical-list.component';
+import { NavigationListComponent } from './components/navigation-list/navigation-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VerticalListComponent
+    VerticalListComponent,
+    NavigationListComponent
   ],
   imports: [
     BrowserModule,
